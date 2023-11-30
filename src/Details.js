cf8768914f34f554793c3fd33ee35c70109690f2
Josesdio <132412719+Josesdio@users.noreply.github.com>
2023-11-30 15:01:49 +0700

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,112 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Josesdio",
+  tagline: "I build things for web, Mobile, and Application",
+  img: profile,
+  about: `Meet Josesdio, a talented individual born in the vibrant city of Surabaya on December 30, 2003. With blood type O coursing through their veins, they embody a blend of creativity and technical prowess. At a young age, Josesdio has already established themselves as a skilled programmer, proficient in a variety of languages. Their expertise spans Swift and Xcode for seamless iOS app development, Java for robust and versatile applications, and the dynamic duo of HTML and CSS for crafting visually appealing and responsive web designs. But that's not all—[Your Name] is also well-versed in React JS, enabling them to build interactive and user-friendly web interfaces. Additionally, they have embraced the world of cross-platform app development with proficiency in Flutter. Beyond their technical proficiency, Josesdio is a forward-thinker, always eager to explore new technologies and stay on the cutting edge of the ever-evolving tech landscape. With a passion for problem-solving and a keen eye for detail, they are poised to make a significant impact in the world of software development. As Josesdio continues to grow and evolve, there's no doubt that their innovative spirit and diverse skill set will shape the future of technology.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/josesdio/",
+  github: "https://github.com/Josesdio",
+  facebook: "https://www.facebook.com/josesdio.dio/",
+  instagram: "https://www.instagram.com/josesdio/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Mobile Developer, Web Developer",
+    College: `Binus University`,
+    Location: "Indonesia",
+    Type: "Undergraduated",
+    Duration: "September 2021 - present",
+  }
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Computer Science",
+    Skills: "ReactJS, Swift, Flutter, JAVA",
+    University: "Binus University",
+    Degree: "Bachelor",
+    Duration: "September 2021 - Present",
+  }
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  swift: swift,
+  flutter: flutter,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  github: github,
+  figma: figma,
+  xcode: xcode,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Blaze Buster AR",
+    image: projectImage1,
+    description: `Blaze Buster AR is an innovative Augmented Reality (AR) app designed to train and improve your firefighting skills through realistic simulations. With diverse fire scenarios, guided tutorials, progress tracking, and multiplayer challenges, users can learn and practice using fire extinguishers in a safe and engaging way. 
+    Suitable for beginners to professionals, Blaze Buster AR is the ultimate training tool for mastering firefighting techniques and promoting safety.`,
+    techstack: "UnityHub, C#",
+  },
+  {
+    title: "NutTracker",
+    image: projectImage2,
+    description: `NutTracker is an application designed to assist users in adopting a healthy lifestyle through a holistic approach, incorporating exercise, diet, and meal planning. The app provides various features that support users on their health journey, offering tools necessary to track and manage key aspects of their healthy lifestyle.`,
+    previewLink: "https://www.figma.com/file/XblNXoAfT8kBGuupXQRrfy/UIUX-NutTracker",
+    
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "josesdio@binus.ac.id",
+  phone: "+62 813-3410-2692",
+  Line: "josesdio"
+};
